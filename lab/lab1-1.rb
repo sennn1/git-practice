c422cf1f214c689904ca3981f1d83fad418850c3
sennn <senayyemane@outlook.com>
2017-07-12 17:58:46 +0700

    basic setup

---
+++ lab/lab1-1.rb
@@ -0,0 +1,9 @@
+puts  "=" * 10 + "Milestone A1" + "=" * 10
+
+puts "How many hours are there in a year?"
+
+
+
+# * How many minutes are there in a decade?
+# * How many seconds old are you?
+# * If Bob is 1246 million seconds old, how old is Bob in years?
---
+++ lab1-2.rb
@@ -0,0 +1,21 @@
+puts "=" * 10 + Milestone A1 + "=" * 10
+
+
+
+puts "many hours are there in a year?"
+hours_day= 
+
+* How many minutes are there in a decade?
+* How many seconds old are you?
+* If Bob is 1246 million seconds old, how old is Bob in years?
+
+
+
+
+
+
+
+
+
+
+
