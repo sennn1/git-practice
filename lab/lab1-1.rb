69160cf2703e23150b3ae9bba7f7618a0aea0fbc
sennn <senayyemane@outlook.com>
2017-07-13 07:05:40 +0700

    milestone a1

--- lab/lab1-1.rb
+++ lab/lab1-1.rb
@@ -1,9 +1,48 @@
 puts  "=" * 10 + "Milestone A1" + "=" * 10
 
 puts "How many hours are there in a year?"
+hours_day = 24
+hours_month = hours_day * 30
+puts hours_month
+hours_year = hours_month * 12
+puts "hours_year:#{hours_year}"
+
+
+puts  "=" * 10 + "" + "=" * 10
 
 
 
-# * How many minutes are there in a decade?
-# * How many seconds old are you?
-# * If Bob is 1246 million seconds old, how old is Bob in years?
+puts "How many minutes are there in a decade?"
+minutes_hour= 60
+minutes_day= minutes_hour * 24
+minutes_month = minutes_day * 30
+minutes_year = minutes_month * 12
+minutes_decade = minutes_year * 10
+
+
+puts  "=" * 10 + "" + "=" * 10
+
+
+puts "How many seconds old are you?"
+seconds_minute = 60
+seconds_hour = seconds_minute * 60
+seconds_day = seconds_hour * 24
+seconds_month = seconds_day * 30
+seconds_year = seconds_month * 12
+seconds_age = seconds_year * 33
+
+
+puts  "=" * 10 + "" + "=" * 10
+
+
+puts "If Bob is 1246 million seconds old, how old is Bob in years?"
+bob_age_seconds = 1_246_000_000 
+seconds_minute = 1 * 60
+seconds_hour = seconds_minute * 60
+seconds_day = seconds_hour * 24
+seconds_month = seconds_day * 30
+seconds_year = seconds_month * 12
+
+
+
+
